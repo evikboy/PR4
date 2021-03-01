69c45f84c75b13816791677ad71b6f02f8f9b022
evikboy <Kislenko1950@gmail.com>
2021-03-01 17:31:30 +0200

    PR4

---
+++ PR4/PR4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int n;
+            n = 10;
+            double[] height, weight;
+            height = new double[n];
+            weight = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                height[i] = rand.Next(149, 199);
+                weight[i] = rand.Next(40, 110);
+            }
+            Console.WriteLine("----------------Неотсортированый массив---------------");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0} работник: рост - {1}, вес - {2}", i + 1, height[i], weight[i]);
+            }
+
+            Array.Sort(height, weight);
+
+            Console.WriteLine("");
+            Console.WriteLine("----------------Отсортированый массив---------------");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0} работник: рост - {1}, вес - {2}", i + 1, height[i], weight[i]);
+            }
+        }
+    }
+}
---
+++ PR4_2/PR4_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string[] name;
+            double[] price;
+            name = new string[] { "Samsung Portable SSD T5", "Kingston SSDnow A400" , "Samsung 970 Evo series" , "Kingston SSD KC 600", "Kingston SSD KC 2500",
+            "Samsung PM 883" , "Kingston DC 500R", "Goodram SSD CL 100"};
+            price = new double[name.Length];
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                price[i] = rand.Next(700, 25000);
+            }
+            Console.WriteLine("----------------Неотсортированый массив---------------");
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("{0} SSD: имя - {1}, цена - {2}", i + 1, name[i], price[i]);
+            }
+
+            Array.Sort(price, name);
+
+            Console.WriteLine("");
+            Console.WriteLine("----------------Отсортированый массив---------------");
+
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("{0} SSD: имя - {1}, цена - {2}", i + 1, name[i], price[i]);
+            }
+        }
+    }
+}
